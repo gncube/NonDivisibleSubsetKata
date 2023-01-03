d8fe391459b4390aa725c701c1f32c0d013aa7b4
GNcube <4821668+gncube@users.noreply.github.com>
2023-01-03 12:43:02 +0000

    solved the problem

--- NonDivisibleSubset.UnitTests/NonDivisibleSubsetProcessor.cs
+++ NonDivisibleSubset.UnitTests/NonDivisibleSubsetProcessor.cs
@@ -8,7 +8,26 @@
 
         internal int LongestSubset(int k, List<int> s)
         {
-            return 3;
+            int[] remainders = new int[k];
+
+            foreach (int number in s)
+            {
+                remainders[number % k]++;
+            }
+
+            int count = Math.Min(remainders[0], 1);
+
+            for (int i = 1; i <= k / 2 && i != k - i; i++)
+            {
+                count += Math.Max(remainders[i], remainders[k - i]);
+            }
+
+            if (k % 2 == 0)
+            {
+                count++;
+            }
+
+            return count;
         }
     }
 }
--- NonDivisibleSubset.UnitTests/NonDivisibleSubsetShould.cs
+++ NonDivisibleSubset.UnitTests/NonDivisibleSubsetShould.cs
@@ -3,7 +3,7 @@ namespace NonDivisibleSubset.UnitTests
     public class NonDivisibleSubsetShould
     {
         [Fact]
-        public void ReturnTheLengthOfTheLongestSubset()
+        public void ReturnTheLengthOfTheLongestSubset1()
         {
             List<int> S = new List<int> { 19, 10, 12, 10, 24, 25, 22 };
             int k = 4;
@@ -13,6 +13,18 @@ namespace NonDivisibleSubset.UnitTests
             int result = processor.LongestSubset(k, S);
 
             int expected = 3;
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(3, 4, new int[] { 19, 10, 12, 10, 24, 25, 22 })]
+        [InlineData(3, 3, new int[] { 1, 7, 2, 4 })]
+        public void ReturnTheLengthOfTheLongestSubset(int expected, int k, int[] S)
+        {
+            var processor = new NonDivisibleSubsetProcessor();
+
+            int result = processor.LongestSubset(k, S.ToList());
 
             Assert.Equal(expected, result);
         }
